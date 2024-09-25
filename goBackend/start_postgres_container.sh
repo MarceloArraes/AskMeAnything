834730ead6e3feb9e345eeb39baea82a1175f1f1
Marcelo <marcelo.arraes@gmail.com>
2024-09-25 20:19:55 +0930

    fix: name db convention

--- goBackend/start_postgres_container.sh
+++ goBackend/start_postgres_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Step 1: Build the Docker images
+echo "pulling"
+git pull
+
 echo "Building Docker images..."
 docker-compose build
 
@@ -10,7 +13,7 @@ docker-compose up -d
 
 # Step 3: Wait for PostgreSQL to be ready
 echo "Waiting for PostgreSQL to be ready..."
-until docker exec -it $(docker ps -q -f name=db) pg_isready > /dev/null 2>&1; do
+until docker exec -it $(docker ps -q -f "name=*db*") pg_isready > /dev/null 2>&1; do
   echo "PostgreSQL is not ready yet. Checking logs..."
   docker logs db  # Output the logs for debugging
   sleep 1
